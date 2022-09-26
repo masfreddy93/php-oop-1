b49b1c5b0e59f3a173c435a0a5a98b319787b8c8
masfreddy93 <masieronicola@gmail.com>
2022-09-26 17:21:27 +0200

    add some style

--- bonus/app/Movie.php
+++ bonus/app/Movie.php
@@ -6,14 +6,16 @@ class Movie {
     public $running_time;
     public $original_language;
     public $vote;
+    public $poster;
 
-    function __construct($title, $directed_by, $running_time, $original_language, $vote)
+    function __construct($title, $directed_by, $running_time, $original_language, $vote, $poster)
     {
         $this->title = $title;
         $this->directed_by = $directed_by;
         $this->running_time = $running_time;
         $this->original_language = $original_language;
         $this->voteIsNumeric($vote);
+        $this->poster = $poster;
     }
 
     public function voteIsNumeric($vote){
--- bonus/index.php
+++ bonus/index.php
@@ -1,17 +1,56 @@
-<?php 
-    
+<?php
+
 include __DIR__ . '/app/Movie.php';
 
 
-$bambi = new Movie('Bambi', 'Aron Arone', 117, 'de', 4.2);
-// $bambi -> title = "Bambi";
-// $bambi -> directed_by = "Aron Arone";
-// $bambi -> running_time = 117;
-// $bambi -> original_language = "de";
-// $bambi -> vote = 4.2;
-
-$ryan = new Movie('Saving Private Ryan', 'Steven Spielberg', 169, 'en', 'ottimo');
+$bambi = (array) new Movie('Bambi', 'Aron Arone', 117, 'de', 4.2, 'https://www.corriere.it/methode_image/2022/08/13/Spettacoli/Foto%20Spettacoli%20-%20Trattate/bambi%204-keqG-U33601694680386CYG-656x492@Corriere-Web-Sezioni.jpg');
+$ryan = (array) new Movie('Saving Private Ryan', 'Steven Spielberg', 169, 'en', 4.6, 'https://pad.mymovies.it/filmclub/2006/01/051/locandina.jpg');
+$inception = (array) new Movie('Inception', 'Cristopher Nolan', 147, 'en', 4.5, 'https://m.media-amazon.com/images/I/912AErFSBHL._SL1500_.jpg');
+$titanic = (array) new Movie('Titanic', 'James Cameron', 182, 'en', 4.9, 'https://www.superguidatv.it/wp-content/uploads/2019/10/titanic-film.jpg');
 
 
+// var_dump($bambi, $ryan, $inception, $titanic);
+// $bambi = (array) $bambi;
 
-var_dump($bambi, $ryan);
+
+
+$movies = [$bambi, $ryan, $inception, $titanic];
+// var_dump($movies);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bonus</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+    <div class="container">
+        <h2>Lista film</h2>
+        <ul>
+            <?php
+            foreach ($movies as $movie) {
+            ?>
+                <li class="card">
+                    <img src="<?= $movie['poster'] ?>" alt="">
+                    <h4> <?= $movie['title'] ?></h4>
+                    <h5> Directed by: <span><?= $movie['directed_by'] ?></span></h5>
+                    <h5> Running time: <span><?= $movie['running_time'] ?></span></h5>
+                    <h5> Original lang: <span><?= $movie['original_language'] ?></span></h5>
+                    <h5> Vote: <span><?= $movie['vote'] ?></span></h5>
+                </li>
+            <?php
+            }
+            ?>
+        </ul>
+    </div>
+
+</body>
+
+</html>
