067422d3a5d5e0b734bc2371b8c1af3f1210fae0
masfreddy93 <masieronicola@gmail.com>
2022-09-26 15:18:21 +0200

    create class Movie + define some variables + create 2 objects

--- index.php
+++ index.php
@@ -1,2 +1,26 @@
 <?php 
-    echo "Hello world";
+    
+class Movie {
+    public $title;
+    public $directed_by;
+    public $running_time;
+    public $original_language;
+    public $vote;
+}
+
+$bambi = new Movie();
+$bambi -> title = "Bambi";
+$bambi -> directed_by = "Aron Arone";
+$bambi -> running_time = 117;
+$bambi -> original_language = "de";
+$bambi -> vote = 4.2;
+
+$ryan = new Movie();
+$ryan -> title = "Saving Private Ryan";
+$ryan -> directed_by = "Steven Spielberg";
+$ryan -> running_time = 169;
+$ryan -> original_language = "en";
+$ryan -> vote = 4.5;
+
+
+var_dump($bambi, $ryan);
